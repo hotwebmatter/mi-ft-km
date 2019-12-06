04c6299ca1d6cbe407f83084735287c624521849
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-05 19:15:44 -0500

    Initial commit of mi-ft-km project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mi_ft_km
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
