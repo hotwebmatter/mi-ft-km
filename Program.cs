783f9a3dd58d5703c076956864573844e1bd4b46
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-05 19:17:17 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace mi_ft_km
 {
@@ -6,7 +7,7 @@ namespace mi_ft_km
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
